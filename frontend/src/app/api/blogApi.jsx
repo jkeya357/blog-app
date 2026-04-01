e9050f9ce323fc88e5a3942a597b11fa05dba751
Joao <jkeya357@gmail.com>
2026-04-01 11:40:54 +0200

    improved backend url

--- frontend/src/app/api/blogApi.jsx
+++ frontend/src/app/api/blogApi.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logout } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.BACKEND_URL,
+  baseUrl: process.env.VITE_BACKEND_URL,
   credentials: 'include',
   prepareHeaders: (headers, {getState}) => {
     const token = getState().auth.token
