0c667c52f366d968d43f9d4e09216910501ac690
Joao <jkeya357@gmail.com>
2026-04-01 11:46:38 +0200

    vite config import

--- frontend/src/app/api/blogApi.jsx
+++ frontend/src/app/api/blogApi.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logout } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.VITE_BACKEND_URL,
+  baseUrl: import.meta.env.VITE_BACKEND_URL,
   credentials: 'include',
   prepareHeaders: (headers, {getState}) => {
     const token = getState().auth.token
